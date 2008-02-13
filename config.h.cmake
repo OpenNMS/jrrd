28ac331232cce8c41e7e6c08c612c984ee88fcd1
Benjamin Reed <rangerrick@users.sourceforge.net>
2008-02-13 19:02:18 +0000

    forgot the config.h file

---
+++ config.h.cmake
@@ -0,0 +1,12 @@
+/* Define to 1 if you have the <unistd.h> header file. */
+#cmakedefine HAVE_UNISTD_H
+
+/* Define to 1 if you have the <string.h> header file. */
+#cmakedefine HAVE_STRING_H
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#cmakedefine HAVE_STDLIB_H
+
+/* Define to 1 if you have the <getopt.h> header file. */
+#cmakedefine HAVE_GETOPT_H
+
